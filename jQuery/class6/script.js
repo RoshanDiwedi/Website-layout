9be91f52cd5a3722c47db4e22d5d14eaa95c45a3
RoshanDiwedi <sroshan154@gmail.com>
2022-05-02 00:11:04 +0530

    get mothod  video completed

---
+++ jQuery/class6/script.js
@@ -0,0 +1,10 @@
+// Window Scroll and Resize function
+$("document").ready(function(){
+    $(window).scroll(function(){
+        console.log("You are scrolling")
+    });
+
+    $(window).resize(function(){
+        console.log("You are resizing")
+    });
+});
---
+++ jQuery/class7/script.js
@@ -0,0 +1,27 @@
+// Get methods 
+$("document").ready(function(){
+
+    // HTML Method
+    var a =  $(".box").html();
+    console.log(a);
+
+    // TEXT Mothod
+    var b = $(".box").text();
+    console.log(b);
+
+    // Attribut Mothod ( attr() )
+    var c = $(".box").attr("class");
+    console.log(c);
+});
+
+
+
+// Val method
+$("document").ready(function(){
+    $("#form").submit(function(){
+        var name = $("#name").val();
+        var cla = $("#class").val();
+        var country = $("#country").val();
+        alert("Hello "+ name +" " + cla +" "+ country);   
+    });
+});
