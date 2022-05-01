b95c2ff97ba0dd81bc9287dd9a8dc891c5a9a4db
roshan diwedi <sroshan154@gmail.com>
2022-05-01 14:57:24 +0530

    jQearyclass1

---
+++ jQuery/jquery.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     alert('Hello world');
+// });
+
+$(document).ready(function(){
+    console.log('hello');
+});
+
+$(document).ready(function(){
+    var a = $('.box-1').html()
+        console.log(a);
+})
