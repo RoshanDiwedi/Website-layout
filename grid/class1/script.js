b10b9fa5e9d4f84554992d57606607f4d908ac98
RoshanDiwedi <sroshan154@gmail.com>
2022-05-09 00:23:48 +0530

    all the folders

---
+++ grid/class1/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".form input").focus(function(){
+        $(".form input").css("outline", "0");
+    });
+
+    $(".fa-solid").on("click", function(){
+        $("#mobile-nav").toggleClass("mobile-nav-block")
+        $("body").css("background", "red")
+    });
+    
+});
